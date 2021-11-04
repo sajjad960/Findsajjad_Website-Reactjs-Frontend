b93740d782a8c4b9776850b3bd29352e7c8d6348
sajjad960 <sajjadhossainsb01@gmail.com>
2021-11-04 23:35:30 +0600

    start working on hero design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello I am from react</h1>
+      <Router>
+      <Switch>
+        <Route exact path="/">
+         
+        </Route>
+        <Route exact path="/about">
+          
+        </Route>
+    
+        <Route exact path="/cart">
+         
+        </Route>
+        <Route exact path="/products">
+         
+        </Route>
+        
+        <Route path="*">
+        
+        </Route>
+      </Switch>
+    
+    </Router>
     </div>
   );
 }
