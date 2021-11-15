85759a85c57cbd52ca0555ababa7e750a60fc368
sajjad960 <sajjadhossainsb01@gmail.com>
2021-11-15 20:57:25 +0600

    working on mecard text area

--- src/components/MeCard.js
+++ src/components/MeCard.js
@@ -3,12 +3,16 @@ import styled from 'styled-components'
 import me from '../assets/img/me/me.jpeg'
 
 export const MeCard = () => {
+
     return (
         <Wrapper>
             <div className="grid">
 
                 <div className="photo_area">
+                    <h2>I am MERN stack developer</h2>
+                    <hr className='hr'/>
                     <img src={me} className='me' alt="Me" />
+                 
                 </div>
 
                 <div className="social_links">
@@ -18,21 +22,38 @@ export const MeCard = () => {
             </div>
 
             <div className="text_area">
-                
+                <div className='text_area-box'>
+                    Hi!
+                    Me = Sajjad 🤪. <br/>
+                    I can work as a Frontend and Backend developer.
+                </div>
             </div>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    padding: 6rem 10rem 1rem 20rem;
+    padding: 3.5% 3% .5% 4%;
+
+    .hr {
+        width: 2rem;
+        margin-bottom: 1rem;
+        margin-left: 2rem;
+        margin-top: .3rem;
+        transform: all 0.4s;
+        animation: widthBig 2s;
+        animation-fill-mode: forwards;
+        animation-timing-function: ease;
+       
+    }
+
 
     .photo_area {
-        height: 30rem;
-        width: 30rem;
+       
+        text-align: center;
     }
     .me {
-        width: 100%;
+        width: 55%;
     }
     .grid {
         display: grid;
@@ -40,5 +61,15 @@ const Wrapper = styled.div`
         grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
         
     }
+
+    //animation
+
+    @keyframes widthBig {
+    0% {
+      opacity: 1;
+      width: 3%; }
+    100% {
+      opacity: 1;
+      width: 90%; } }
     
 `
